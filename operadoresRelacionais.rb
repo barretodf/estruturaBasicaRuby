d42345c38901f185c2d51a498855dd8fa4898cb0
Edmilson Barreto <edsb1981@gmail.com>
2025-02-02 21:50:14 -0300

    Operadores Relacionais

---
+++ operadoresRelacionais.rb
@@ -0,0 +1,11 @@
+puts "Digite um número:"
+x = gets.to_i
+puts "Digite outro número:"
+y = gets.to_i
+
+puts "x > y? #{x > y}"
+puts "x < y? #{x < y}"
+puts "x == y? #{x == y}"
+puts "x != y? #{x != y}"
+
+
